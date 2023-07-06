94f3f27baa243a88f72ad83c3883994e0e6e5369
Teodor Heggelund <git@teod.eu>
2023-07-06 16:55:51 +0200

    read suite

---
+++ testsuite/runsuite.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#
+# Usage:
+#
+#     ./runsuite.py JSONSUITE INTERPRETER
+#
+# To run with the privovided JSON suite and the provided interpreter:
+#
+#     ./runsuite.py suite.json "python ../zepi.py repl"
+
+import json
+import sys
+
+def read_json_file(path):
+    with open(path, "r") as f:
+        jsondata = json.loads(f.read())
+    return jsondata
+
+print(read_json_file(sys.argv[1]))
